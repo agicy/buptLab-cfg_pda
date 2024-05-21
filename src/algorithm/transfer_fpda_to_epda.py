84efa845603d93b594dbc083779ebf3763ab541d
Sherlocked_hzoi <Sherlocked_hzoi@163.com>
2024-05-21 13:15:39 +0800

    feat: implement algorithm for converting final state PDA to empty stack PDA
    - Developed an algorithm to convert a final state Pushdown Automaton (PDA) to an empty stack PDA.
    - Ensured the algorithm adheres to theoretical principles and correctness.
    - Improved the PDA by enabling conversion to an empty stack acceptance condition.

--- src/algorithm/transfer_fpda_to_epda.py
+++ src/algorithm/transfer_fpda_to_epda.py
@@ -4,4 +4,31 @@ __all__: list[str] = ["transfer_fpda_to_epda"]
 
 
 def transfer_fpda_to_epda(fpda: PDA) -> PDA:
-    return PDA(Q=set(), T=set(), Gamma=set(), delta={}, q0="q0", Z0="Z", F=set())
+
+    epda_q0 = fpda.q0 + "'"
+    epda_Z0 = fpda.Z0 + "'"
+    epda_qf = fpda.q0 + "''"
+
+    epda_Q = fpda.Q | {epda_q0, epda_qf}
+    epda_T = fpda.T
+    epda_Gamma = fpda.Gamma | {epda_Z0}
+    epda_delta = fpda.delta
+    epda_F = set[state_t]()
+
+    epda_delta[(epda_q0, "", epda_Z0)] = {(fpda.q0, (fpda.Z0, epda_Z0))}
+    for Z in epda_Gamma:
+        for f in fpda.F:
+            if (f, "", Z) not in epda_delta:
+                epda_delta[(f, "", Z)] = set()
+            epda_delta[(f, "", Z)].add((epda_qf, ()))
+        epda_delta[(epda_qf, "", Z)] = {(epda_qf, ())}
+
+    return PDA(
+        Q=epda_Q,
+        T=epda_T,
+        Gamma=epda_Gamma,
+        delta=epda_delta,
+        q0=epda_q0,
+        Z0=epda_Z0,
+        F=epda_F,
+    )
